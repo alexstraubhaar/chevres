e23623e8af427093797f9c396873079f9dda767c
Bintjeman <gmile.caume@gmail.com>
2018-10-24 21:49:21 +0200

    ajout de bornes pour la vitesse et la position ; ce qui permettra de mieux travailler sur la gui...

--- src/Simulation/ADessiner.cpp
+++ src/Simulation/ADessiner.cpp
@@ -7,17 +7,17 @@
 // constantes à changer en parametres
 const sf::Color couleurCercleNourriture = sf::Color(20,255,20); //Vert moche
 const float rayonDeNourriture = 5.f;
-const int nbreDeNourritures = 1000;
+const int nbreDeNourritures = 10;
 const float minimum = 0.f, maximum = 1000.f;
 const float minAcceleration = -500.f, maxAcceleration = 500.f;
 
 ADessiner::ADessiner()
 {
-    generateurAleaF alea0_1000f(minimum, maximum);
+    generateurAleaF aleaf(minimum, maximum);
 
     for(int i = 0 ; i < nbreDeNourritures ; ++i)
     {
-        sf::Vector2f position(alea0_1000f.alea(), alea0_1000f.alea());
+        sf::Vector2f position(aleaf.alea(), aleaf.alea());
         std::cout << "x : " << position.x << " ; y : " << position.y << "\n"; // Debug ; à supprimer
         sf::CircleShape rondNourrirure(rayonDeNourriture);
 
@@ -31,7 +31,6 @@ ADessiner::ADessiner()
 
 void ADessiner::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-
     for(int i = 0 ; i < nourritures.size() ; ++i)
     {
         target.draw(nourritures[i], states);
@@ -39,16 +38,32 @@ void ADessiner::draw(sf::RenderTarget& target, sf::RenderStates states) const
 }
 void ADessiner::petitTour()
 {
-    generateurAleaF alea0_1000f(minAcceleration, maxAcceleration);
+    generateurAleaF aleaf(minAcceleration, maxAcceleration);
 
     for(int i = 0 ; i < nourritures.size() ; ++i)
     {
-        sf::Vector2f acceleration(alea0_1000f.alea()/10000, (alea0_1000f.alea()/10000));
-        vitesses[i].x += acceleration.x;
-        vitesses[i].y += acceleration.y;
-
-        nourritures[i].setPosition(
-            nourritures[i].getPosition().x + vitesses[i].x,
-            nourritures[i].getPosition().y + vitesses[i].y);
+        sf::Vector2f acceleration(aleaf.alea()/100000, (aleaf.alea()/100000));
+        vitesses[i] += acceleration;
+        if(vitesses[i].x > 1){
+            vitesses[i].x = 1;
+        }
+        if(vitesses[i].y > 1){
+            vitesses[i].y = 1;
+        }
+        if(vitesses[i].x < -1){
+            vitesses[i].x = -1;
+        }
+        if(vitesses[i].y < -1){
+            vitesses[i].y = -1;
+        }
+        nourritures[i].setPosition(nourritures[i].getPosition() + vitesses[i]);
+        if(nourritures[i].getPosition().x < 0 || nourritures[i].getPosition().x > 1000)
+        {
+            vitesses[i].x *= -1;
+        }
+        if(nourritures[i].getPosition().y < 0 || nourritures[i].getPosition().y > 1000)
+        {
+            vitesses[i].y *= -1;
+        }
     }
 }
